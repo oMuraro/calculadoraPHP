ccdfbc75fcf4d7536a4aca09128528c5e0ea440b
M3NT0Sz <matheusmendes2005@outlook.com>
2024-05-15 14:01:48 -0300

    a

---
+++ logica/processamento.php
@@ -0,0 +1,27 @@
+<?php
+    require "funcoesCalculo.php";
+
+    $resultado = "";
+    session_start(); //iniciando a session permitindo usu de variaveis de session
+
+    if(!empty($_GET['inputNum1']) && !empty($_GET['inputNum2'])){
+        $numeros1 = $_GET['inputNum1'];
+        $numeros2 = $_GET['inputNum2'];
+
+        if($_GET['selectOperacoes'] == "adicao"){
+            $resultado = $numeros1  . " + " . $numeros2 . " = " . soma($numeros1, $numeros2);
+        } else if($_GET['selectOperacoes'] == "subtracao"){
+            $resultado = $numeros1  . " - " . $numeros2 . " = " . subtracao($numeros1, $numeros2);
+        } else if($_GET['selectOperacoes'] == "multiplicacao"){
+            $resultado = $numeros1  . " x " . $numeros2 . " = " . multiplicacao($numeros1, $numeros2);
+        } else {
+            $resultado = $numeros1  . " / " . $numeros2 . " = " . divisao($numeros1, $numeros2);
+        }
+        $_SESSION['resultado'] = $resultado;
+        echo $resultado;
+
+        header("location:../index.php");
+        die();
+    }
+
+?>
